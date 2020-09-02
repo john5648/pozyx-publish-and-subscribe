02f228a117481c7516b2bb2d6f199770d7f40f84
joohyun lee <john5648@naver.com>
2020-09-02 13:53:18 +0900

    straight line

--- src/listener.py
+++ src/listener.py
@@ -1,120 +1,107 @@
 #!/usr/bin/env python
-
 import rospy
 from std_msgs.msg import Float32
-from nav_msgs.msg import Odometry
-from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Point, Twist
-from math import atan2, pi
+from math import pi, sqrt
 import numpy as np
 
 pozyx_x=0.0
 pozyx_y=0.0
 
-x = 0.0
-y = 0.0
-theta = 0.0
-
 def callback1(data):
     global pozyx_x
     pozyx_x=data.data
-
 def callback2(data):
     global pozyx_y
     pozyx_y=data.data
 
-def newOdom (msg):
-    global x
-    global y
-    global theta
-
-    x = msg.pose.pose.position.x
-    y = msg.pose.pose.position.y
-    
-    rot_q = msg.pose.pose.orientation
-    (roll, pitch, theta) = euler_from_quaternion ([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
-
 def angle(rotate_angle):
-    global x
-    global y
-    global theta
     global flag
-
-    target_radian= theta+rotate_angle
-    if target_radian > pi:
-        target_radian=target_radian-2*pi
+    #rotate speed is 30
+    angular_speed = 30*2*pi/360
+    relative_angle = rotate_angle*2*pi/360
+    current_angle = 0.0
 
     speed.linear.x = 0.0
-    while abs(target_radian-theta)>0.1:
-        speed.angular.z = 0.4
+    speed.angular.z = -abs(angular_speed)
+
+    t0 = rospy.Time.now().to_sec()
+    # multplying 1.158 is to correct the real world angle
+    while(current_angle < relative_angle*1.158):
         pub.publish(speed)
-        r.sleep()
-    flag=1
-
-def drive(goal_distance):
-    global x
-    global y
-    global flag
-
+        t1 = rospy.Time.now().to_sec()
+        current_angle = angular_speed*(t1-t0)
+    speed.linear.x = 0.0
     speed.angular.z = 0.0
-    flag_2=0
-    last_pos=[x,y]
-    goal_distance=goal_distance**2
+    pub.publish(speed)
+    r.sleep()
 
-    while((x-last_pos[0])**2+(y-last_pos[1])**2 < goal_distance):
-        speed.linear.x = 0.5
-        if flag_2%6==5:
-            speed.angular.z = -0.03
-        else:
-            speed.angular.z = 0.0
-
-        pub.publish(speed)      
-        r.sleep()
-        flag_2=flag_2+1
-    flag=0
-
-def ro_cir():
-    global x
-    global y
+def along_line():
     global flag
-    global i
-
-    dir_count=0
-    neg_flag=0
-
-    speed.linear.x = 0.2
-    speed.angular.z = 0.4
-
+    global target_loc
+    global current_loc    
+    #time_counter lets jackal turn angular.z=0.4 for only 10 times and 20 times with angular.z=0.1
+    # to prevent overshooting
+    time_counter=0
+    speed.angular.z = 0.0
+    speed.linear.x = 0.0
+    criteriion_dis=sqrt((current_loc[0]-target_loc[0])**2 + (current_loc[1]-target_loc[1])**2)
     while(True):
+        [eqt, remain_dis, drive_dis] = criterion()
+        if eqt>=400 and time_counter<=10:
+            speed.linear.x = 0.3
+            speed.angular.z = -0.4
+            time_counter=time_counter+1
+        elif eqt<-400 and time_counter<=10:
+            speed.linear.x = 0.3
+            speed.angular.z = 0.4  
+            time_counter=time_counter+1   
+        elif eqt>=400 and time_counter>10:
+            speed.linear.x = 0.4
+            speed.angular.z = -0.1
+            time_counter=time_counter+1           
+        elif eqt<-400 and time_counter>10:
+            speed.linear.x = 0.4
+            speed.angular.z = 0.1  
+            time_counter=time_counter+1  
+        elif eqt >= 0 and eqt<400:
+            speed.linear.x = 0.4
+            speed.angular.z = -0.1
+            time_counter=0
+        elif eqt < 0 and eqt>=-400:
+            speed.linear.x = 0.4
+            speed.angular.z = 0.1
+            time_counter=0
+        time_counter=time_counter%20
+        
+        if remain_dis<200 or drive_dis>=criteriion_dis:
+            break
+
         pub.publish(speed)      
-        r.sleep()
-	
-	a=criterion()
-
-	i=i+1
-	if a>=0:
-	    dir_count=dir_count+1
-	elif a<0:
-	    dir_count=dir_count-1
-	
-	
-
-def criterion():
-    global x
-    global y
-    global flag
+        r.sleep()   
+        
+    speed.linear.x = 0.0
+    speed.angular.z = 0.0
+    pub.publish(speed)
+    r.sleep()  
+    flag=5
     
-    eqt=pozyx_y-current_loc[1]-(target_loc[1]-current_loc[1])/(target_loc[0]-current_loc[0])*(pozyx_x-current_loc[0])
+def criterion():
+    global target_loc
+    global current_loc
+    global pozyx_x
+    global pozyx_y
+    linear_eqt=pozyx_y-current_loc[1]-(target_loc[1]-current_loc[1])/(target_loc[0]-current_loc[0])*(pozyx_x-current_loc[0])
 
-    return eqt
+    remain_dis= sqrt((target_loc[0]-pozyx_x)**2 + (target_loc[1]-pozyx_y)**2)
+    drive_dis= sqrt((current_loc[0]-pozyx_x)**2 + (current_loc[1]-pozyx_y)**2)
+
+    return linear_eqt, remain_dis, drive_dis
 
 
 rospy.init_node('listener', anonymous=True)
-
 sub1= rospy.Subscriber('/chatter1', Float32, callback1)
 sub2= rospy.Subscriber('/chatter2', Float32, callback2)
-
-sub = rospy.Subscriber("/odometry/filtered", Odometry, newOdom)
 pub = rospy.Publisher("/cmd_vel",Twist, queue_size=1)
 
 speed = Twist()
@@ -122,30 +109,32 @@ speed = Twist()
 #sleeping rate 
 r=rospy.Rate(10) #10hz
 
-flag=4
-
-i=1
-
-
-tar_angle=pi*51/100 #0.51
-
+flag=3
 #pozyx need some time to set up
 #rospy.sleep(3)
 
-current_loc=[1300,2000]
-target_loc=[4000,4000]
+current_loc=[600,600]
+target_loc=[3000,3000]
 
 while not rospy.is_shutdown():
-    TOA=np.array([[0,0]])
-    TOA[0][0]=pozyx_x
-    TOA[0][1]=pozyx_y
-    print(TOA)
+
     if flag==0:
-        angle(tar_angle)
+        angle(90)
     elif flag==1:
-        drive(7.2)
+        along_line()
+    elif flag==2:
+        print('done')
     elif flag==3:
-        ro_cir()
-    elif flag==4:
-        print(TOA)
+        i=0
+        ii=0
+        while(i<30):
+            speed.linear.x = i
+            pub.publish(speed)
+            r.sleep()
+            ii=ii+1
+            if ii%2==0:
+                i=i+0.02
+        speed.linear.x = 0
+        pub.publish(speed)
+        r.sleep()
 
